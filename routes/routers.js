e99a5d5531a7030bdd77d5c269567512a18b50e9
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2025-02-05 22:09:40 +0530

    first commit

--- routes/routers.js
+++ routes/routers.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const authController = require("../controller/auth_controller");
-// const storeController = require("../controller/stoe_controller");
-
-// router.post("/register", authController.register);
-// router.post("/login", authController.login);
-// router.post("/addstore",storeController.addStore);
-// router.get("/getallstores",storeController.getStores);
-// router.get("/:id",storeController.getStoreById)
-// router.post("/store/:id/items", storeController.addItemToStore);
-// router.get("/stores", storeController.getStores);
-
-
-// module.exports = router;
-
-
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth_controller");
